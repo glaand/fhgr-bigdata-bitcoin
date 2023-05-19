09304ca4b4fcf37e6f1cae78de28e6edbb6d84ed
Deadeye111 <61310625+Deadeye111@users.noreply.github.com>
2023-05-19 11:25:23 +0200

    Update rusty-blockparser-export.sh

--- scripts/rusty-blockparser-export.sh
+++ scripts/rusty-blockparser-export.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script exports all blocks and transactions
 # export PATH="$HOME/.cargo/bin:$PATH"  # sets path variable for the cargo bin folder where rusty-blockparser is located
-rusty-blockparser -d ~/raw-data/.bitcoin/blocks -t 16 balances ~/processed-data/rusty-dump  # creates balances.csv -> address ; balance
-rusty-blockparser -d ~/raw-data/.bitcoin/blocks -t 16 unspentcsvdump ~/processed-data/rusty-dump  # creates unspent.csv -> txid ; indexOut ; height ; value ; address
-rusty-blockparser -d ~/raw-data/.bitcoin/blocks -t 16 csvdump ~/processed-data/rusty-dump # creates big csvs with all data -> blocks.csv, transactions.csv, tx_in.csv, tx_out.csv
-rusty-blockparser -d ~/raw-data/.bitcoin/blocks -t 16 opreturn
+rusty-blockparser -d mounted-data/raw-data/blocks -t 16 balances mounted-data/processed-data/rusty-dump  # creates balances.csv -> address ; balance
+rusty-blockparser -d mounted-data/raw-data/blocks -t 16 unspentcsvdump mounted-data/processed-data/rusty-dump  # creates unspent.csv -> txid ; indexOu>
+rusty-blockparser -d mounted-data/raw-data/blocks -t 16 csvdump mounted-data/processed-data/rusty-dump # creates big csvs with all data -> blocks.csv,>
+rusty-blockparser -d mounted-data/raw-data/blocks -t 16 opreturn
