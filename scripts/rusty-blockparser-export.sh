d1e50fc3c024d56d9df162d52283008ba49b0162
Deadeye111 <61310625+Deadeye111@users.noreply.github.com>
2023-05-19 10:45:43 +0200

    Update rusty-blockparser-export.sh

--- scripts/rusty-blockparser-export.sh
+++ scripts/rusty-blockparser-export.sh
@@ -4,4 +4,4 @@
 rusty-blockparser -d ~/raw-data/.bitcoin/blocks -t 16 balances ~/processed-data/rusty-dump  # creates balances.csv -> address ; balance
 rusty-blockparser -d ~/raw-data/.bitcoin/blocks -t 16 unspentcsvdump ~/processed-data/rusty-dump  # creates unspent.csv -> txid ; indexOut ; height ; value ; address
 rusty-blockparser -d ~/raw-data/.bitcoin/blocks -t 16 csvdump ~/processed-data/rusty-dump # creates big csvs with all data -> blocks.csv, transactions.csv, tx_in.csv, tx_out.csv
-rusty-blockparser -d ~/raw-data/.bitcoin/blocks -t 16 opreturn ~/processed-data/rusty-dump
+rusty-blockparser -d ~/raw-data/.bitcoin/blocks -t 16 opreturn
