3ad7d184a0972b46803be93266d811ca11c6955f
Deadeye111 <61310625+Deadeye111@users.noreply.github.com>
2023-05-19 14:54:45 +0200

    Update rusty-blockparser-export.sh

--- scripts/rusty-blockparser-export.sh
+++ scripts/rusty-blockparser-export.sh
@@ -3,5 +3,5 @@
 # export PATH="$HOME/.cargo/bin:$PATH"  # sets path variable for the cargo bin folder where rusty-blockparser is located
 rusty-blockparser --start 0 --end 790000 -d mounted-data/raw-data/blocks balances mounted-data/processed-data/rusty-dump  # creates balances.csv
 rusty-blockparser --start 0 --end 790000 -d mounted-data/raw-data/blocks unspentcsvdump mounted-data/processed-data/rusty-dump  # creates unspent.csv
-rusty-blockparser --start 0 --end 790000 -d mounted-data/raw-data/blocks -t 16 csvdump mounted-data/processed-data/rusty-dump # creates big csvs with all data
+rusty-blockparser --start 0 --end 790000 -d mounted-data/raw-data/blocks csvdump mounted-data/processed-data/rusty-dump # creates big csvs with all data
 rusty-blockparser -d mounted-data/raw-data/blocks opreturn
