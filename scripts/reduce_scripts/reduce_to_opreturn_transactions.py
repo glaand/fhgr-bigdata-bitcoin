db53d738fb32a8d7dd050106699abbd7137b391b
Gregorini Leandro <gregorleandr@iridium.fhgr.ch>
2023-05-26 13:37:35 +0200

    changed directory of reduced since there is a typo

--- scripts/reduce_scripts/reduce_to_opreturn_transactions.py
+++ scripts/reduce_scripts/reduce_to_opreturn_transactions.py
@@ -1,31 +1,32 @@
-import os
-from os.path import abspath
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, when
+    import os
+    from os.path import abspath
+    from pyspark.sql import SparkSession
+    from pyspark.sql.functions import col, when
 
-warehouse_location = abspath('spark-warehouse')
+    warehouse_location = abspath('spark-warehouse')
 
-spark = SparkSession \
-    .builder \
-    .appName("SparkByExamples.com") \
-    .config("spark.sql.warehouse.dir", warehouse_location) \
-    .enableHiveSupport() \
-    .getOrCreate()
+    spark = SparkSession \
+        .builder \
+        .appName("SparkByExamples.com") \
+        .config("spark.sql.warehouse.dir", warehouse_location) \
+        .config("spark.driver.memory", "125g") \
+        .enableHiveSupport() \
+        .getOrCreate()
 
-# select db
-spark.sql("USE btc_blockchain")
+    # select db
+    spark.sql("USE btc_blockchain")
 
-daily_transactions_value = spark.sql("""
-    SELECT 
-        COUNT(*),
-        YEAR(blocks.ntime), 
-        MONTH(blocks.ntime), 
-        DAY(blocks.ntime)
-    FROM tx_out_temp
-    JOIN transactions ON transactions.txid = tx_out_temp.txid
-    JOIN blocks b ON b.hash = transactions.blockHash
-    WHERE SUBSTRING(tx_out_temp.script, 1, 2) = '6a'
-    GROUP BY YEAR(blocks.ntime), MONTH(blocks.ntime), DAY(blocks.ntime)
-""")
-daily_transactions_value.show()
-daily_transactions_value.write.csv("/reduced_data/opreturn_transactions.csv")
+    daily_transactions_value = spark.sql("""
+        SELECT 
+            COUNT(*),
+            YEAR(b.nTime), 
+            MONTH(b.nTime), 
+            DAY(b.nTime)
+        FROM tx_out_temp
+        JOIN transactions ON transactions.txid = tx_out_temp.txid
+        JOIN blocks b ON b.block_hash = transactions.hashBlock
+        WHERE SUBSTRING(tx_out_temp.scriptPubKey, 1, 2) = '6a'
+        GROUP BY YEAR(b.nTime), MONTH(b.nTime), DAY(b.nTime)
+    """)
+    daily_transactions_value.show()
+    daily_transactions_value.write.csv("/reduced-data/opreturn_transactions.csv")
